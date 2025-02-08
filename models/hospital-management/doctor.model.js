aad54018ca169149938638fe90e95a54146d79ee
Anjitkumaryadav <anjitaniket@gmail.com>
2025-02-08 15:11:05 +0530

    model: hospital

--- models/hospital-management/doctor.model.js
+++ models/hospital-management/doctor.model.js
@@ -1,5 +1,26 @@
 const mongoose = require ("mongoose")
 
-const doctorSchema = new mongoose.Schema({},{timestamps:true})
+const doctorSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    salary: {
+        type: String,
+        required: true
+    },
+    qualifications:{
+        type: String,
+        required: true
+    },
+    experienceInYear: {
+        type: Number,
+        default: 0
+    },
+    worksInHospitals: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Hospital"
+    }
+},{timestamps:true})
 
 export const Doctor = mongoose.model("Doctor", doctorSchema)
--- models/hospital-management/hospital.model.js
+++ models/hospital-management/hospital.model.js
@@ -1,5 +1,31 @@
 const mongoose = require("mongoose")
 
-const hospitalSchema = new mongoose.Schema({},{timestamps:true})
+const hospitalSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    addressLine2:{
+        type:String,
+        required: true,
+    },
+    city:{
+        type: String,
+        required: true
+    },
+    pincode:{
+        type: String,
+        required: true
+    },
+    specialized: [
+        {
+        type:String,
+        }
+    ]
+},{timestamps:true})
 
 export const Hospital = mongoose.model("Hospital",hospitalSchema)
